2722c0498580c0d23ddd2d433f402fa131aaa03e
Rafael Carvalho <rapha.pse@outlook.com>
2016-09-13 22:28:12 -0300

    novo app somador

--- Calcula_Mamadeira/app/src/main/java/br/com/rcc/calcula_mamadeira/MainActivity.java
+++ Calcula_Mamadeira/app/src/main/java/br/com/rcc/calcula_mamadeira/MainActivity.java
@@ -1,13 +1,84 @@
 package br.com.rcc.calcula_mamadeira;
 
+import android.support.annotation.FloatRange;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    CheckBox ck_60, ck_90, ck_120, ck_150, ck_180, ck_210, ck_240, ck_270, ck_300;
+    Button btnCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ck_60 = (CheckBox) findViewById(R. id. ck_60);
+        ck_90 = (CheckBox) findViewById(R. id. ck_90);
+        ck_120 = (CheckBox) findViewById(R. id. ck_120);
+        ck_150 = (CheckBox) findViewById(R. id. ck_150);
+        ck_180 = (CheckBox) findViewById(R. id. ck_180);
+        ck_210 = (CheckBox) findViewById(R. id. ck_210);
+        ck_240 = (CheckBox) findViewById(R. id. ck_240);
+        ck_270 = (CheckBox) findViewById(R. id. ck_270);
+        ck_300 = (CheckBox) findViewById(R. id. ck_300);
+
+        btnCalc = (Button) findViewById(R. id. btnCalc);
+
+        btnCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+               int tota = 0;
+
+                if(ck_60.isChecked()){
+                    total += 2;
+                }
+                if(ck_90.isChecked()){
+                    total += 3;
+                }
+                if(ck_120.isChecked()){
+                   total += 4;
+                }
+                if(ck_150.isChecked()){
+                    total += 5;
+                }
+                if(ck_180.isChecked()){
+                    total += 6;
+                }
+                if(ck_210.isChecked()){
+                    total += 7;
+                }
+                if(ck_240.isChecked()){
+                    total += 8;
+                }
+                if(ck_270.isChecked()){
+                    total += 9;
+                }
+                if(ck_300.isChecked()){
+                    total += 10;
+                }
+
+
+
+
+                AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+                alerta.setTitle("Aviso");
+                alerta.setMessage(+String.valueOf(total)+ " Colheres");
+
+                alerta.setNeutralButton("OK", null);
+                alerta.show();
+
+            }
+        });
+
+
+
     }
 }
--- RafaelCarvalho/app/src/main/java/br/com/rcc/rafaelcarvalho/MainActivity.java
+++ RafaelCarvalho/app/src/main/java/br/com/rcc/rafaelcarvalho/MainActivity.java
@@ -29,18 +29,19 @@ public class MainActivity extends AppCompatActivity {
                 float n2 = 75;
 
                 n1 = Float.parseFloat(etNum1.getText().toString());
-                n2 = Float.parseFloat(getText().toString());
-                res = n1 / n2;
+                String str = Float.toString(n2);
+                n2 = Float.parseFloat(n2.getText().toString());
+                res = n1 / str;
 
                 String strRes = String.valueOf(res);
                 etRes.setText(strRes);
 
-                /*
+
                 AlertDialog.Builder caixaAlerta = new AlertDialog.Builder(MainActivity.this);
                 caixaAlerta.setMessage("Pronto!!!!");
 
                 caixaAlerta.show();
-                */
+
 
             }
         });
