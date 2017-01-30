ce80486718f686171ff02713a871e258b67c21fe
Rafael <rapha.pse@outlook.com>
2017-01-30 11:41:25 -0200

    App mudando tela

---
+++ MudandoTela/app/src/main/java/com/example/developer/mudandotela/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.developer.mudandotela;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        Button btn2 = (Button) findViewById(R.id.btn2);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.segundatela);
+            }
+        });
+    }
+    public void chamaTela(View v){
+        setContentView(R. layout. segundatela);
+    }
+}
