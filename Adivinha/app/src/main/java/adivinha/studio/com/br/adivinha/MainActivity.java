9ac4ba670bd1e0c4b140f2f707fa372cc68f708a
Rafael <rapha.pse@outlook.com>
2017-06-12 20:48:30 -0300

    Criando jogo Adivinha - com números randomicos

--- Adivinha/app/src/main/java/adivinha/studio/com/br/adivinha/MainActivity.java
+++ Adivinha/app/src/main/java/adivinha/studio/com/br/adivinha/MainActivity.java
@@ -2,8 +2,11 @@ package adivinha.studio.com.br.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +19,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         btnJogar = (Button) findViewById(R.id.btn);
         textoResultado = (TextView) findViewById(R.id.resultado);
 
+       // textoResultado.setText("Texto Alterado ... ");
 
-        textoResultado.setText("Texto Alterado ... ");
+        btnJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random rd = new Random();
+
+                int numeroAleatorio = rd.nextInt(10);
+
+                textoResultado.setText("Número escolhido é: "+numeroAleatorio);
+            }
+        });
 
 
 
