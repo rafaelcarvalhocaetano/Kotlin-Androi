4e4c7c508cefcfe08c441528453d9bd60634afbf
Rafael <rapha.pse@outlook.com>
2017-06-12 19:41:12 -0300

    Adicionando app - alterando estrututa através do strings.xml

---
+++ AppInfo/app/src/main/java/studio/com/br/appinfo/MainActivity.java
@@ -0,0 +1,13 @@
+package studio.com.br.appinfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
