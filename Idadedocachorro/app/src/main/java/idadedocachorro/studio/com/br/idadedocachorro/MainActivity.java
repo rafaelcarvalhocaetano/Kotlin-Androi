226b422c7f9bcc9928f667e409dcb614d32c5ce9
Rafael <rapha.pse@outlook.com>
2017-06-14 21:14:53 -0300

    App calc idade do cão

--- Idadedocachorro/app/src/main/java/idadedocachorro/studio/com/br/idadedocachorro/MainActivity.java
+++ Idadedocachorro/app/src/main/java/idadedocachorro/studio/com/br/idadedocachorro/MainActivity.java
@@ -2,6 +2,7 @@ package idadedocachorro.studio.com.br.idadedocachorro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -20,5 +21,27 @@ public class MainActivity extends AppCompatActivity {
         resultado = (TextView) findViewById(R.id.resultado);
         btnIdade = (Button) findViewById(R.id.btnIdade);
         idade = (EditText) findViewById(R.id.numero);
+
+
+        btnIdade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //recuperando o que foi digitado
+               String textoDigitado = idade.getText().toString();
+
+                if(textoDigitado.isEmpty()){
+                    //Mostrar mensagem de erro
+                    resultado.setText("Insira um valor .... ");
+
+                }else{
+
+                    int valorDigitado = Integer.parseInt(textoDigitado);
+                    int resultadoFinal = valorDigitado * 7;
+
+                    resultado.setText("A idade do cachorro em anos humanos é: "+resultadoFinal+" Anos");
+
+                }
+            }
+        });
     }
 }
