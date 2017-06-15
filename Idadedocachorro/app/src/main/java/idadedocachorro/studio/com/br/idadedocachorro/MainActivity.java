729f12b467a3df7134f83f7c4d490d5dacbf8614
Rafael <rapha.pse@outlook.com>
2017-06-14 21:05:39 -0300

    App calc idade do cão

--- Idadedocachorro/app/src/main/java/idadedocachorro/studio/com/br/idadedocachorro/MainActivity.java
+++ Idadedocachorro/app/src/main/java/idadedocachorro/studio/com/br/idadedocachorro/MainActivity.java
@@ -2,12 +2,23 @@ package idadedocachorro.studio.com.br.idadedocachorro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultado;
+    private Button btnIdade;
+    private EditText idade;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resultado = (TextView) findViewById(R.id.resultado);
+        btnIdade = (Button) findViewById(R.id.btnIdade);
+        idade = (EditText) findViewById(R.id.numero);
     }
 }
