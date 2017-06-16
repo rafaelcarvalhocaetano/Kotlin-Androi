686fe945c4da5f86677981584faa769ca875d9ac
Rafael <rapha.pse@outlook.com>
2017-06-16 20:23:33 -0300

    Adicionando App ERP

--- ERP/app/src/main/java/erp/studio/com/br/erp/MainActivity.java
+++ ERP/app/src/main/java/erp/studio/com/br/erp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -13,6 +14,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Toast.makeText(getApplicationContext(), "ON-CREATE ... ", Toast.LENGTH_LONG).show();
         setContentView(R.layout.activity_main);
 
         botao = (Button) findViewById(R.id.btn1);
@@ -28,30 +30,36 @@ public class MainActivity extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
+        Toast.makeText(getApplicationContext(), "ON-START MÉTODO CHAMADO ... ", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+        Toast.makeText(getApplicationContext(), "ON-RESTART SENDO CHAMADO ... ", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        Toast.makeText(getApplicationContext(), "ON-RESUME SENDO CHAMADO ... ", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        Toast.makeText(getApplicationContext(), "ON-PAUSE SENDO CHAMADO ... ", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+        Toast.makeText(getApplicationContext(), "ON-STOP SENDO CHAMADO ... ", Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Toast.makeText(getApplicationContext(), "ON-DESTROY SENDO CHAMADO ... ", Toast.LENGTH_LONG).show();
     }
 }
