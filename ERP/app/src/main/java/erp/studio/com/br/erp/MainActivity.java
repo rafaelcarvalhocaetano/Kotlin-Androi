97ebf852b4e9bb8552751bff5e2026636c59f34f
Rafael <rapha.pse@outlook.com>
2017-06-16 20:02:07 -0300

    Adicionando App ERP

---
+++ ERP/app/src/main/java/erp/studio/com/br/erp/MainActivity.java
@@ -0,0 +1,14 @@
+package erp.studio.com.br.erp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
