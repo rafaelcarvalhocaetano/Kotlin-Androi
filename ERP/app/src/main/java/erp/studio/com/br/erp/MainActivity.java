15774f5ac507a8d8495d0133b94e78836405632a
Rafael <rapha.pse@outlook.com>
2017-06-16 20:14:58 -0300

    Adicionando App ERP

--- ERP/app/src/main/java/erp/studio/com/br/erp/MainActivity.java
+++ ERP/app/src/main/java/erp/studio/com/br/erp/MainActivity.java
@@ -1,14 +1,57 @@
 package erp.studio.com.br.erp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    private Button botao;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botao = (Button) findViewById(R.id.btn1);
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SegundaActivity.class));
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
---
+++ ERP/app/src/main/java/erp/studio/com/br/erp/SegundaActivity.java
@@ -0,0 +1,16 @@
+
+
+package erp.studio.com.br.erp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
