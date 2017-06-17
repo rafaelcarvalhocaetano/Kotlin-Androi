ff4928352f1c51ef0d520322aae48340407a45aa
Rafael <rapha.pse@outlook.com>
2017-06-16 21:11:22 -0300

    Empresa de consultoria - ITCON

--- ITConsultoria/app/src/main/java/itconsultoria/studio/com/br/itconsultoria/EmpresaActivity.java
+++ ITConsultoria/app/src/main/java/itconsultoria/studio/com/br/itconsultoria/EmpresaActivity.java
@@ -4,11 +4,11 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class EmpresaActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_empresa);
     }
 }
