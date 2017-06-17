5b651e4f7dffee1708ab349ff98f85938738d720
Rafael <rapha.pse@outlook.com>
2017-06-17 13:50:31 -0300

    Adicionando projeto App de login

---
+++ Login/app/src/main/java/login/studio/com/br/login/SegundaActivity.java
@@ -0,0 +1,13 @@
+package login.studio.com.br.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
