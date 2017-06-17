4f5908d24ec855a872e47cc026061285b8d2556c
Rafael <rapha.pse@outlook.com>
2017-06-17 13:06:17 -0300

    App - Consultoria ATM

--- ATM-Consultoria/app/src/main/java/atmconsultoria/studio/com/br/atm_consultoria/MainActivity.java
+++ ATM-Consultoria/app/src/main/java/atmconsultoria/studio/com/br/atm_consultoria/MainActivity.java
@@ -1,18 +1,27 @@
 package atmconsultoria.studio.com.br.atm_consultoria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button empresa;
+    private ImageView empresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        empresa = (Button) findViewById(R.id.empresa);
+        empresa = (ImageView) findViewById(R.id.empresa);
+
+        empresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Empresa.class));
+            }
+        });
     }
 }
