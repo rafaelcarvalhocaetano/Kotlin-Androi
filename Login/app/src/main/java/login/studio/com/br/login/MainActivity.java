dfe1401b62b12459b9108212b079185e22c47c19
Rafael <rapha.pse@outlook.com>
2017-06-17 13:57:07 -0300

    Adicionando projeto App de login

--- Login/app/src/main/java/login/studio/com/br/login/MainActivity.java
+++ Login/app/src/main/java/login/studio/com/br/login/MainActivity.java
@@ -1,13 +1,29 @@
 package login.studio.com.br.login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        botao = (Button) findViewById(R.id.btnLogar);
+
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SegundaActivity.class));
+            }
+        });
     }
 }
