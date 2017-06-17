c64d371dd40cd522b6676e21198c8fe957844226
Rafael <rapha.pse@outlook.com>
2017-06-17 12:39:27 -0300

    App - Consultoria ATM

--- ATM-Consultoria/app/src/main/java/atmconsultoria/studio/com/br/atm_consultoria/Empresa.java
+++ ATM-Consultoria/app/src/main/java/atmconsultoria/studio/com/br/atm_consultoria/Empresa.java
@@ -2,17 +2,12 @@ package atmconsultoria.studio.com.br.atm_consultoria;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Button empresa;
+public class Empresa extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        empresa = (Button) findViewById(R.id.empresa);
+        setContentView(R.layout.activity_empresa);
     }
 }
