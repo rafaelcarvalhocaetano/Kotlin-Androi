d7f65f72b0e4225b71b427d4133a258e8b0042a5
Rafael <rapha.pse@outlook.com>
2017-06-18 20:21:45 -0300

    Adicionando App que apresenta uma listview

---
+++ Viagem-ListView/app/src/main/java/viagem/studio/com/br/viagem_listview/MainActivity.java
@@ -0,0 +1,13 @@
+package viagem.studio.com.br.viagem_listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
