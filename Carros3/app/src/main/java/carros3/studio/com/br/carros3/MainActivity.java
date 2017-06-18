d513c60d0eb0a39656c7fc6ac4a2aacdb4d68b6e
Rafael <rapha.pse@outlook.com>
2017-06-18 19:40:54 -0300

    Jogo do adivinha quem é

--- Carros3/app/src/main/java/carros3/studio/com/br/carros3/MainActivity.java
+++ Carros3/app/src/main/java/carros3/studio/com/br/carros3/MainActivity.java
@@ -1,13 +1,38 @@
 package carros3.studio.com.br.carros3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView jogar;
+    private String [] opcao = {"cara", "coroa"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        jogar = (ImageView) findViewById(R.id.imagemBotao);
+
+        jogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Random random = new Random();
+                int numAleatorio = random.nextInt(2);
+
+                //opcao[numAleatorio]
+                Intent intent = new Intent(MainActivity.this, JogoActivity.class);
+
+
+            }
+        });
     }
 }
