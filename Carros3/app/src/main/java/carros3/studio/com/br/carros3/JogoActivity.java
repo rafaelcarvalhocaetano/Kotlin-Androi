b99d85071c468c8bf1fd93c1e56aa3b55cb4521c
Rafael <rapha.pse@outlook.com>
2017-06-18 20:14:47 -0300

    Jogo do adivinha - FINALIZADO

--- Carros3/app/src/main/java/carros3/studio/com/br/carros3/JogoActivity.java
+++ Carros3/app/src/main/java/carros3/studio/com/br/carros3/JogoActivity.java
@@ -1,7 +1,10 @@
 package carros3.studio.com.br.carros3;
 
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -16,6 +19,24 @@ public class JogoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_jogo);
 
         botao = (Button) findViewById(R.id.btnVoltar);
-       imagem = (ImageView) findViewById(R.id.imageView3);
+        imagem = (ImageView) findViewById(R.id.img);
+
+        Bundle extra = getIntent().getExtras();
+
+        if(extra != null){
+            String opcaoEscolhida = extra.getString("opcao");
+            if(opcaoEscolhida.equals("cara")){
+                imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.c2));
+            }else{
+                imagem.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.c3));
+            }
+        }
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(JogoActivity.this, MainActivity.class));
+            }
+        });
     }
 }
