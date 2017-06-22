9e18523a8d6e7ecb14b714d03cc4b55bc1ecf230
Rafael <rapha.pse@outlook.com>
2017-06-22 11:04:36 -0300

    Adicionando App com seekbar sobre sereies

---
+++ Series/app/src/main/java/series/studio/com/br/series/MainActivity.java
@@ -0,0 +1,13 @@
+package series.studio.com.br.series;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
