fa7d2499ab3405b2921ca162f1c8ab06256c191f
Rafael <rapha.pse@outlook.com>
2017-06-22 11:31:22 -0300

    Escokhendo a comida - App radiobutton

--- QComida-RadioButton/app/src/main/java/rb/studio/com/br/qcomida_radiobutton/MainActivity.java
+++ QComida-RadioButton/app/src/main/java/rb/studio/com/br/qcomida_radiobutton/MainActivity.java
@@ -2,12 +2,33 @@ package rb.studio.com.br.qcomida_radiobutton;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup rg;
+    private RadioButton escolhido;
+    private Button btn;
+    private TextView texto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rb = (RadioGroup) findViewById(R.id.rg);
+        btn = (Button) findViewById(R.id.btn);
+        texto = (TextView) findViewById(R.id.exibirTexto);
+
+       btn.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               
+           }
+       });
     }
 }
