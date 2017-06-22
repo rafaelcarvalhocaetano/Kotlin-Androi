9f50ee0458e47d740bc87936f4c97430e86f68f5
Rafael <rapha.pse@outlook.com>
2017-06-22 16:21:38 -0300

    App com integração ao Sharedpreferences - Salvando e recuperendo

--- ArmazenamentodeDados/app/src/main/java/db/studio/com/br/armazenamentodedados/MainActivity.java
+++ ArmazenamentodeDados/app/src/main/java/db/studio/com/br/armazenamentodedados/MainActivity.java
@@ -1,13 +1,45 @@
 package db.studio.com.br.armazenamentodedados;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nome;
+    private Button btn;
+    private TextView exibir;
+
+    private static final String ARQUIVO = "ArquivoPreferencia";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nome = (EditText) findViewById(R.id.texto);
+        btn = (Button) findViewById(R.id.btn);
+        exibir = (TextView) findViewById(R.id.exibir);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences sp = getSharedPreferences(ARQUIVO,0);
+                SharedPreferences.Editor editor = sp.edit();
+
+                if(nome.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Por Favor Preencher o Nome !", Toast.LENGTH_SHORT).show();
+                }else{
+                    editor.putString("Nome: "+nome.getText().toString());
+                    editor.commit();
+                }
+            }
+        });
     }
 }
