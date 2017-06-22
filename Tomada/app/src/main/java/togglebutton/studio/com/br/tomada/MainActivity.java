a3b3a3bafda84201fb3a01c3fe1d8c363c3e5e38
Rafael <rapha.pse@outlook.com>
2017-06-22 11:58:57 -0300

    App tomada - toggle button

--- Tomada/app/src/main/java/togglebutton/studio/com/br/tomada/MainActivity.java
+++ Tomada/app/src/main/java/togglebutton/studio/com/br/tomada/MainActivity.java
@@ -2,12 +2,32 @@ package togglebutton.studio.com.br.tomada;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton tb;
+    private TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tb = (ToggleButton) findViewById(R.id.tb);
+        t = (TextView) findViewById(R.id.txt);
+
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                t.setText("");
+                if(isChecked){
+                    t.setText("Mostrar ... ");
+                }
+
+            }
+        });
     }
 }
