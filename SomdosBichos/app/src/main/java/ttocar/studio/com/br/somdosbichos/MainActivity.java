3ebaccbae2a537c7bda587903e65f2f5843f9e49
Rafael <rapha.pse@outlook.com>
2017-06-22 12:24:47 -0300

    App que exibe o som e imagem dos bichos

--- SomdosBichos/app/src/main/java/ttocar/studio/com/br/somdosbichos/MainActivity.java
+++ SomdosBichos/app/src/main/java/ttocar/studio/com/br/somdosbichos/MainActivity.java
@@ -3,6 +3,7 @@ package ttocar.studio.com.br.somdosbichos;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +19,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = (Button) findViewById(R.id.btn);
-        mp = (MediaPlayer) find
+        mp = MediaPlayer.create(MainActivity.this, R.raw.musica);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(mp.isPlaying()){
+                    pausar();
+                }else{
+                    tocarMusica();
+                }
+
+            }
+        });
+    }
+    private void tocarMusica(){
+        if(mp != null){
+            mp.start();
+            btn.setText("Pausar");
+        }
+    }
+    private void pausar(){
+        if(mp != null){
+            mp.pause();
+            btn.setText("Tocar");
+        }
+    }
+    @Override
+    protected void onDestroy(){
+
+        if(mp != null && mp.isPlaying()){
+            mp.stop();
+            mp.release();
+            mp = null;
+        }
+        super.onDestroy();
     }
 }
