1badf88381262360a883cc8e975cb14e6e99a76d
Rafael <rapha.pse@outlook.com>
2017-06-22 11:07:39 -0300

    Adicionando App com seekbar sobre series

--- Series/app/src/main/java/series/studio/com/br/series/MainActivity.java
+++ Series/app/src/main/java/series/studio/com/br/series/MainActivity.java
@@ -2,12 +2,38 @@ package series.studio.com.br.series;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar sb;
+    private ImageView img;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sb = (SeekBar) findViewById(R.id.sb);
+        img = (ImageView) findViewById(R.id.img);
+
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
