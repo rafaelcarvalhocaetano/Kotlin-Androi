59cf84ca905cb0a23810b859dca3a23cad6c7506
Rafael <rapha.pse@outlook.com>
2017-06-22 10:54:10 -0300

    Adicionando App ref seekbar

--- SeekBar/app/src/main/java/sb/studio/com/br/seekbar/MainActivity.java
+++ SeekBar/app/src/main/java/sb/studio/com/br/seekbar/MainActivity.java
@@ -2,12 +2,42 @@ package sb.studio.com.br.seekbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView texto;
+    private TextView texto2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar = (SeekBar) findViewById(R.id.sb);
+        texto = (TextView) findViewById(R.id.txt);
+        texto2 = (TextView) findViewById(R.id.t2);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                texto.setText("Progresso -->  "+i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "SeekBar pressionado ....", Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(MainActivity.this, "Salvando valor ... ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
