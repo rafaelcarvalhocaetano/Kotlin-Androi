af3545b9f51d03e61a94ccc677840142625b4409
Rafael <rapha.pse@outlook.com>
2017-06-22 11:52:12 -0300

    App tomada - toggle button

---
+++ Tomada/app/src/main/java/togglebutton/studio/com/br/tomada/MainActivity.java
@@ -0,0 +1,13 @@
+package togglebutton.studio.com.br.tomada;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
