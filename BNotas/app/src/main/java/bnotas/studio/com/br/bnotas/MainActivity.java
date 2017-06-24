f4f35fb673b5498447109383d84c3088d18148f9
Rafael <rapha.pse@outlook.com>
2017-06-24 20:52:37 -0300

    App bloco de notas

--- BNotas/app/src/main/java/bnotas/studio/com/br/bnotas/MainActivity.java
+++ BNotas/app/src/main/java/bnotas/studio/com/br/bnotas/MainActivity.java
@@ -9,13 +9,18 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView btn;
     private EditText texto;
+    private static final String NOME = "arquivo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +39,48 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //Recuperar
+        if(lerArquivo() != null){
+            texto.setText(lerArquivo());
+        }
     }
     public void gravarArquivo(String t){
         try {
-            OutputStreamWriter out = new OutputStreamWriter(openFileOutput("anotacaotxt", Context.MODE_PRIVATE));
+            OutputStreamWriter ot = new OutputStreamWriter( openFileOutput(NOME, Context.MODE_PRIVATE));
+            ot.write(t);
+            ot.close();
 
         }catch (IOException e){
             Log.v("MainActivity", e.toString());
 
         }
+    }
+    private String lerArquivo(){
+        String result = "";
+        try {
+
+            //Abrir arquivo
+        InputStream arquivo = openFileInput(NOME);
+            if(arquivo != null){
+                //ler arquivo
+                InputStreamReader ip = new InputStreamReader(arquivo);
+
+                //Gera Buffer do arquivo
+                BufferedReader br = new BufferedReader(ip);
+
+                //recuperar texto do arquivo
+                String linhaArquivo = "";
+                while ( (linhaArquivo = br.readLine()) != null){
+
+                    result += linhaArquivo;
+
+                }
+                arquivo.close();
+            }
+
+        }catch (IOException e){
+            Log.v("MainActivity", e.toString());
+        }
+        return result;
     }
 }
