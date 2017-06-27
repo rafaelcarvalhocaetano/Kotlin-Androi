7e9fb24782265bca36c961af6fafecf1121e104b
Rafael <rapha.pse@outlook.com>
2017-06-27 20:51:22 -0300

    Armazenamento no BD

--- BD/app/src/main/java/bd/studio/com/br/bd/MainActivity.java
+++ BD/app/src/main/java/bd/studio/com/br/bd/MainActivity.java
@@ -1,8 +1,10 @@
 package bd.studio.com.br.bd;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+
+
+
         SQLiteDatabase db = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
         //tabela
@@ -18,5 +24,25 @@ public class MainActivity extends AppCompatActivity {
 
         //Inserir dados
         db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Rafael', 31) ");
+        db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Rosemeire', 28) ");
+
+        //Cursor para verificar os itens
+        Cursor cursor = db.rawQuery("SELECT nome, idade FROM pessoas ", null);
+
+        int indiceColunaNome = cursor.getColumnIndex("nome");
+        int indiceColunaIdade = cursor.getColumnIndex("idade");
+
+        cursor.moveToFirst();
+
+        while (cursor != null){
+            Log.i("RESULTADO - nome: ", cursor.getString(indiceColunaNome));
+            Log.i("RESULTADO - idade: ", cursor.getString(indiceColunaIdade));
+            cursor.moveToNext();
+
+        }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
