5fa57cae5f5e3a0bd3b337f8a0a5fec8c9e315fe
Rafael <rapha.pse@outlook.com>
2017-06-27 21:05:05 -0300

    Armazenamento no BD

--- BD/app/src/main/java/bd/studio/com/br/bd/MainActivity.java
+++ BD/app/src/main/java/bd/studio/com/br/bd/MainActivity.java
@@ -20,14 +20,26 @@ public class MainActivity extends AppCompatActivity {
         SQLiteDatabase db = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
         //tabela
-        db.execSQL("CREATE TABLE IF NOT EXISTS pessoas(nome VARCHAR, idade INT(3) ) ");
+        db.execSQL("CREATE TABLE IF NOT EXISTS pessoas(id INTEGER PRIMARY KEY, nome VARCHAR, idade INT(3) ) ");
 
         //Inserir dados
-        db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Rafael', 31) ");
-        db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Rosemeire', 28) ");
+            /*
+        db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Vitor', 8) ");
+        db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Heitor', 3) ");
+        */
+            //UPDATE
+            db.execSQL("UPDATE pessoas SET idade = 20 WHERE nome = Rosemeire ", null);
+            //DELETE
+            db.execSQL("DELETE FROM pessoas WHERE nome = 'Rafael' ");
+            // DROP
+            /*
+             db.execSQL("DROP TABLE pessoas ");
+             */
+
+
 
         //Cursor para verificar os itens
-        Cursor cursor = db.rawQuery("SELECT nome, idade FROM pessoas ", null);
+        Cursor cursor = db.rawQuery("SELECT nome, idade FROM pessoas WHERE nome LIKE '%ra%' ", null);
 
         int indiceColunaNome = cursor.getColumnIndex("nome");
         int indiceColunaIdade = cursor.getColumnIndex("idade");
