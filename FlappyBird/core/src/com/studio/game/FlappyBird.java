e8139e251965b4cabc0581c781397258d1e5990b
Rafael <rapha.pse@outlook.com>
2017-06-28 20:28:32 -0300

    Adicionando jogo - clone Flappy Bird

--- FlappyBird/core/src/com/studio/game/FlappyBird.java
+++ FlappyBird/core/src/com/studio/game/FlappyBird.java
@@ -7,27 +7,19 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class FlappyBird extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private int contador = 0;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Gdx.app.log("Create", "Inicializado o jogo");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		cotador ++;
+		Gdx.app.log("Render", "Renderizando o jogo"+contador);
 	}
 	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
