163fbf3c6f98f9b4538ac7811eec828445972c4e
Rafael <rapha.pse@outlook.com>
2017-06-27 21:13:12 -0300

    Armazenamento no BD

--- BD/app/src/main/java/bd/studio/com/br/bd/MainActivity.java
+++ BD/app/src/main/java/bd/studio/com/br/bd/MainActivity.java
@@ -20,35 +20,39 @@ public class MainActivity extends AppCompatActivity {
         SQLiteDatabase db = openOrCreateDatabase("app", MODE_PRIVATE, null);
 
         //tabela
-        db.execSQL("CREATE TABLE IF NOT EXISTS pessoas(id INTEGER PRIMARY KEY, nome VARCHAR, idade INT(3) ) ");
+        db.execSQL("CREATE TABLE IF NOT EXISTS pessoas(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INT(3) ) ");
 
-        //Inserir dados
             /*
+        //Inserir dados
+
         db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Vitor', 8) ");
         db.execSQL("INSERT INTO pessoas (nome, idade) VALUES ('Heitor', 3) ");
-        */
+
+
             //UPDATE
             db.execSQL("UPDATE pessoas SET idade = 20 WHERE nome = Rosemeire ", null);
             //DELETE
             db.execSQL("DELETE FROM pessoas WHERE nome = 'Rafael' ");
             // DROP
-            /*
+
              db.execSQL("DROP TABLE pessoas ");
-             */
+        */
 
 
 
         //Cursor para verificar os itens
-        Cursor cursor = db.rawQuery("SELECT nome, idade FROM pessoas WHERE nome LIKE '%ra%' ", null);
+        Cursor cursor = db.rawQuery("SELECT * FROM pessoas ", null);
 
         int indiceColunaNome = cursor.getColumnIndex("nome");
         int indiceColunaIdade = cursor.getColumnIndex("idade");
+            int indiceColunaID = cursor.getColumnIndex("id");
 
         cursor.moveToFirst();
 
         while (cursor != null){
-            Log.i("RESULTADO - nome: ", cursor.getString(indiceColunaNome));
-            Log.i("RESULTADO - idade: ", cursor.getString(indiceColunaIdade));
+            Log.i("RESULTADO - NOME: ", cursor.getString(indiceColunaNome));
+            Log.i("RESULTADO - IDADE: ", cursor.getString(indiceColunaIdade));
+            Log.i("RESULTADO - ID: ", cursor.getString(indiceColunaID));
             cursor.moveToNext();
 
         }
