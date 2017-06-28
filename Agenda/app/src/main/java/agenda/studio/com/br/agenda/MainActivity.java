dbb236b2866655277b1d9623e0fc3a7cf188d5b0
Rafael <rapha.pse@outlook.com>
2017-06-27 21:39:56 -0300

    App CRUD de uma Agenda

--- Agenda/app/src/main/java/agenda/studio/com/br/agenda/MainActivity.java
+++ Agenda/app/src/main/java/agenda/studio/com/br/agenda/MainActivity.java
@@ -1,13 +1,67 @@
 package agenda.studio.com.br.agenda;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText texto;
+    private Button btn;
+    private ListView lista;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try {
+
+        //recuperar componentes
+        texto = (EditText) findViewById(R.id.texto);
+        btn = (Button) findViewById(R.id.btn);
+        lista = (ListView) findViewById(R.id.lista);
+
+        //Banco de dados
+        db = openOrCreateDatabase("apptarefas", MODE_PRIVATE, null);
+
+        //tabela tarefas
+        db.execSQL("CREATE TABLE IF NOT EXISTS tarefas (id INTEGER PRIMARY KEY AUTOINCREMENT, tarefa VARCHAR )");
+
+        //
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String textoDigitado = texto.getText().toString();
+
+                db.execSQL("INSERT INTO tarefas (tarefa) VALUES(' "+ textoDigitado +" ') ");
+            }
+        });
+
+            //Recupera as tarefas
+            Cursor cursor = db.rawQuery("SELECT * FROM tarefas", null);
+
+            //recupera os id das colunas
+            int indiceColunaId = cursor.getColumnIndex("id");
+            int indiceColunaTarefa = cursor.getColumnIndex("tarefa");
+
+            //Lista as tarefas
+            cursor.moveToFirst();
+
+            while(cursor != null){
+                Log.i("RESULTADO - ", "Tarefa: "+cursor.getString(indiceColunaTarefa));
+                cursor.moveToNext();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
