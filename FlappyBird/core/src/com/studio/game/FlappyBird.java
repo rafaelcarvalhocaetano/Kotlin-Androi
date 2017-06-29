454a3bef5a8636d6a2ff8d4b3572ee84f0599f27
Rafael <rapha.pse@outlook.com>
2017-06-29 10:45:46 -0300

    Jogo Flappy bird

--- FlappyBird/core/src/com/studio/game/FlappyBird.java
+++ FlappyBird/core/src/com/studio/game/FlappyBird.java
@@ -11,6 +11,8 @@ public class FlappyBird extends ApplicationAdapter {
 	private Texture[] passaros;
 	private SpriteBatch sb;
 	private Texture fundo;
+	private Texture canoAlto;
+	private Texture canoBaixo;
 
 	//Atributos de configuração
 	private int movimento = 0;
@@ -19,6 +21,8 @@ public class FlappyBird extends ApplicationAdapter {
 	private float variacao = 0;
 	private float velocidadeQueda = 0;
 	private float posicaoInicialVerticar;
+	private float movimentoCanoHorizontal = larguraDispositivo;
+	private float espacoCanos;
 
 
 	@Override
@@ -31,11 +35,17 @@ public class FlappyBird extends ApplicationAdapter {
 		passaros[1] = new Texture("passaro2.png");
 		passaros[2] = new Texture("passaro3.png");
 
+		//Exibindo os canos
+		canoBaixo = new Texture("cano_baixo_maior.png");
+		canoAlto = new Texture("cano_topo_maior.png");
 		fundo = new Texture("fundo.png");
+		//espaço entre os canos
+		espacoCanos = 300;
 
 		larguraDispositivo = Gdx.graphics.getWidth();
 		alturaDispositivo = Gdx.graphics.getHeight();
 		posicaoInicialVerticar = alturaDispositivo / 2;
+		movimentoCanoHorizontal = larguraDispositivo - 100;
 
 	}
 
@@ -49,11 +59,11 @@ public class FlappyBird extends ApplicationAdapter {
 		if(variacao > 2){
 			variacao = 0;
 		}
-
+		//Fazendo o passaro subir com o toque na tela
 		if(Gdx.input.justTouched()){
 			velocidadeQueda = -10;
 		}
-
+		//Fazendo ele descer caso não seja tocado na tela
 		if(posicaoInicialVerticar > 0 || velocidadeQueda < 0){
 			posicaoInicialVerticar = posicaoInicialVerticar - velocidadeQueda;
 		}
@@ -61,8 +71,16 @@ public class FlappyBird extends ApplicationAdapter {
 
 
 		sb.begin();
+		//largura do dispositivo
 		sb.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
+		//Fazendo o passaro se movimentar
 		sb.draw(passaros[(int) variacao], 30, posicaoInicialVerticar);
+
+		//posicionamento dos canos alto
+		sb.draw(canoAlto, movimentoCanoHorizontal,alturaDispositivo / 2 + espacoCanos / 2);
+		//posicionamento cano baixo
+		sb.draw(canoBaixo, movimentoCanoHorizontal, alturaDispositivo / 2 - canoBaixo.getHeight());
+
 
 		sb.end();
 	}
