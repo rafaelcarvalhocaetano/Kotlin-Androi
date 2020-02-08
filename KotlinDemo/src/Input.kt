0e19c24d8e88ee4f88329b7d0a56c461d6eec1ea
Rafael Carvalho <rapha.pse@outlook.com>
2020-02-08 17:58:32 -0300

    Value for Input readLine()
    Signed-off-by: Rafael Carvalho <rapha.pse@outlook.com>

---
+++ KotlinDemo/src/Input.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+//    INPUT
+
+    val name: String = readLine()!!.toString();
+    val age: Int = readLine()!!.toInt();
+    val GPA: Double = readLine()!!.toDouble();
+
+    println("Your name : $name ");
+    println("Your age : $age ");
+    println("Your GPA : $GPA ");
+}
