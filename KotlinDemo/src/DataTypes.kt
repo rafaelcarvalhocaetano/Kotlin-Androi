7765b6e4bc2ec7c7266d96def04014b87b8aa8be
Rafael Carvalho <rapha.pse@outlook.com>
2020-02-08 17:50:34 -0300

    Variables type and values alternative
    Signed-off-by: Rafael Carvalho <rapha.pse@outlook.com>

--- KotlinDemo/src/DataTypes.kt
+++ KotlinDemo/src/DataTypes.kt
@@ -1,3 +1,15 @@
 fun main() {
 
+    val str: String;
+    val age = 10;
+    val ageTwo = 10.6;
+    str = "Test";
+    val data: Double;
+
+    println(str);
+    println(age);
+    println(ageTwo);
+    println("$age Test de New $ageTwo Novo Val $str");
+    data = 10.2;
+    println(data);
 }
