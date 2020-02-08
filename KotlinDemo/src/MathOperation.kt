6a1eb546a800a313423a79583eaa4a6a8277ae3a
Rafael <rapha.pse@outlook.com>
2020-02-08 18:20:19 -0300

    Operation
    Signed-off-by: Rafael Carvalho <rapha.pse@outlook.com>

---
+++ KotlinDemo/src/MathOperation.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val number1 = 20;
+    val number2 = 2;
+
+    val sum: Int = number1 + number2;
+    println("+ Sum $sum");
+
+    val sub: Int = number1 - number2;
+    println("+ Sum $sub");
+
+    val mult: Int = number1 * number2;
+    println("+ Sum $mult");
+
+    val div: Int = number1 / number2;
+    println("+ Sum $div");
+}
---
+++ KotlinDemo/src/SwapNumber.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var number1: Int = readLine()!!.toInt();
+    var number2: Int = readLine()!!.toInt();
+
+    number1 = number2;
+
+    println("Number one $number1 and $number2");
+}
